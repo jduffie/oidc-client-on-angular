b504bc5163053c2dc87374d1150540844ee15c81
John Duffie <john.duffie@xyleminc.com>
2020-11-17 16:01:42 -0500

    add component to protect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProtectedComponent} from './protected/protected.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: []
+  },
+  {
+    path: 'protected',
+    component: ProtectedComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProtectedComponent } from './protected/protected.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProtectedComponent
   ],
   imports: [
     BrowserModule,
